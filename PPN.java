bfe20b6662cd269eb226df6adc585e4c8e4ba03f
Putri Naila Salsabila <putrinailasalsa@gmail.com>
2025-01-03 20:43:32 +0700

    Create PPN.java

---
+++ PPN.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class PPN {
+    void PPNmobil(){
+        ArrayList<Integer> price = Menu.price;
+        ArrayList<Integer> ppn = Menu.ppn;
+
+        for (int i = 0; i < price.size(); i++){
+            ppn.add((price.get(i) * 12 / 100) + price.get(i)); 
+        }
+    }
+}
