e8a05441677ef87bf84b6e27a3f543b347a47bcb
Putri Naila Salsabila <putrinailasalsa@gmail.com>
2025-01-03 21:30:56 +0700

    Update Inputcarlist.java

--- Inputcarlist.java
+++ Inputcarlist.java
@@ -2,5 +2,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Inputcarlist {
-    void inputingcarlist() {}
+    void inputingcarlist() {
+        ArrayList<Integer> price = Menu.price;
+        ArrayList<String> brand = Menu.brand;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("############ Input Car Brand ##########");
+        brand.add(input.next());
+
+        System.out.println("############ Input Car Price ##########");
+        price.add(input.nextInt());
+    }
 }
